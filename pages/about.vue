6c36e485c9cdbd00c985327dd9937b6e9dda3d06
MiyukiShirasawa <miyuki.shirasawa@colabo-ya.jp>
2020-03-22 13:23:15 +0900

    #119当サイトについての追記

--- pages/about.vue
+++ pages/about.vue
@@ -4,8 +4,12 @@
       当サイトについて
     </h2>
     <TextCard>
-      当サイトは、千葉県とCivicTechZenChibaの組織を超えた共業のもと、新型コロナウィルス感染症（COVID-19）における公式情報と客観的な数値をわかりやすく伝え、千葉県にお住まいの方、企業の方、観光に訪れる方が、現状を把握して適切な対策を取ることができるようにすることを目的としています。<br />
-      開発は、シビックテック団体<a href="https://sites.google.com/view/civictechzenchiba/"_blank" rel="noopener">CivicTechZenChiba</a>が行っています。
+        当サイトは、千葉県とCivicTechZenChibaの組織を超えた共業のもと、新型コロナウィルス感染症（COVID-19）における公式情報と客観的な数値をわかりやすく伝え、千葉県にお住まいの方、企業の方、観光に訪れる方が、現状を把握して適切な対策を取ることができるようにすることを目的としています。<br />
+      開発は、シビックテック団体<a href="https://sites.google.com/view/civictechzenchiba/" target="_blank" rel="noopener">CivicTechZenChiba</a>が行っています。<br />
+        複製・改変が許されたオープンソースライセンスで公開されている、
+        <a href="https://stopcovid19.metro.tokyo.lg.jp/" target="_blank" rel="noopener">東京都公式新型コロナウイルス対策サイト</a>の
+        <a href="https://github.com/tokyo-metropolitan-gov/covid19" target="_blank" rel="noopener">仕組み</a>
+        を利用しています。
     </TextCard>
     <TextCard title="ブラウザ環境について">
       当サイトは以下の環境でご覧いただくことを推奨いたします。<br />
